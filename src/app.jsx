a0a998c0918eaf87a45f270ea8a0e4ca1e0ef1bc
texjnr-cloud <texjnr@gmail.com>
2025-11-30 12:44:19 +0000

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PrioritizationTool from './components/PrioritizationTool';
+
+export default function App() {
+  return (
+    <div>
+      <PrioritizationTool />
+    </div>
+  );
+}
